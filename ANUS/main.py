c6f1f49e83c68a9283600b59a837117a0d6115a5
Jonathan Turner <turner@atlantissrv.com>
2025-03-13 12:28:58 -0400

    initcommit

---
+++ ANUS/main.py
@@ -0,0 +1,34 @@
+import sys
+
+from PyQt6.QtCore import QSize
+from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import QApplication
+from panels.main_window import MainWindow
+import time
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    # Show loading screen
+    loading_screen = LoadingScreen()
+    loading_screen.show()
+
+    # Initialize the main window
+    main_window = MainWindow()
+    screen_size = app.primaryScreen().size()
+    main_window.resize(int(screen_size.width()/1.25), int(screen_size.height()/1.5))
+    main_window.setWindowTitle("IcarusEye - Drone Object Detection System")
+
+    main_window.setWindowIcon(QIcon("resources/icons/Icarus Icon.ico"))
+
+    time.sleep(1)
+    # Hide loading screen and show main window
+    loading_screen.close()
+    main_window.show()
+
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
